81f881ef12345a390529476cdfde19c24b2d610b
164895942 <audraz@epidataconsulting.com>
2022-08-24 13:04:47 -0400

    primer commit

---
+++ js/convertir.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	
+	$(document).on('click', '#previewHtml', function(){
+		previewHTMLFile();
+	});
+	
+	$(document).on('click', '#convertHtmlToPDF', function(){
+		converHTMLToPDF();
+	});
+	
+});
+
+function previewHTMLFile() {	
+	var filePath = $('#fileUpload').get(0).files[0];	
+	var fileContents;
+	$("#error-message").html("");
+	$("#fileUpload").css("border", "#a6a6a6 1px solid");
+	if ($(filePath).length != 0) {
+		var reader = new FileReader();
+		reader.onload = function(e) {			
+			fileContents = e.target.result;			
+			$(".preview").show();
+			$("#previewHtmlContent").html(fileContents);			
+			$("#previewHtml").addClass('hidden');
+			$("#convertHtmlToPDF").removeClass('hidden');
+			$(".preview").removeClass('hidden');
+			$(".preview").addClass("borde");
+			
+		}
+		reader.readAsText(filePath);
+	} else {
+		$("#error-message").html("required.").show();
+		$("#fileUpload").css("border", "#d96557 1px solid");
+	}
+}
+
+function converHTMLToPDF() {
+	const { jsPDF } = window.jspdf;
+	var pdf = new jsPDF('p', 'pt', 'a4');
+	var pdfjs = document.querySelector('#previewHtmlContent');		
+	pdf.html(pdfjs, {
+		callback: function(pdf) {
+			pdf.save("output.pdf");
+		}
+	});
+}
